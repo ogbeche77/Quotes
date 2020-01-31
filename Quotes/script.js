a71fcc88da6f0786e15af5509023525c57551695
ogbeche77 <ogbeche77@yahoo.com>
2020-01-31 11:17:31 +0100

    formatting

---
+++ Quotes/script.js
@@ -0,0 +1,40 @@
+var famousQuotes = [
+{
+     name:"author number 1",
+      quote: "quote number 1",
+},
+
+{
+     name:"author number 2",
+     quote: "quote number 2",
+},
+
+{
+     name:"author number 1",
+     quote: "quote number 1",
+},
+
+{
+     name:"author number 1",
+     quote: "quote number 1",
+},
+
+{
+     name:"author number 1",
+     quote: "quote number 1",
+},
+{
+     name:"author number 1",
+     quote: "quote number 1",
+}
+
+
+
+
+
+
+
+
+
+
+]
